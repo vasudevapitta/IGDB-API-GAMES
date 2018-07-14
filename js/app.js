2e84766b59e7cb6bbd1d17dc3b44c6e78dd16297
Vasudeva-Pitta <tizzicboy@gmail.com>
2018-07-14 08:50:18 -0400

    polyfill adjustments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 $(()=>{
 
+/*##################################################################################################################*/
+/*---------------------------------------------------POLYFILL-------------------------------------------------------*/
+    const availableTags = $('#games-list').find('option').map(function () {
+      return this.value;
+    }).get();
+    $('#games-input').autocomplete({ source: availableTags });
+
+    var nativedatalist = !!('list' in document.createElement('input')) && 
+      !!(document.createElement('datalist') && window.HTMLDataListElement);
+  
+    if (!nativedatalist) {
+      $('input[list]').each(function () {
+        var availableTags = $('#' + $(this).attr("list")).find('option').map(function () {
+          return this.value;
+        }).get();
+        $(this).autocomplete({ source: availableTags });
+      });
+    }
+/*##################################################################################################################*/
+
+
 const num = $("#num");
 const userKey = "c61eb3e89ac035974c4dbebfa783422e";
 const contentType = "application/json";
